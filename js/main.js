dc720e407ae54f310f53bea17646f6d033520318
Zahira <zai.alvarez.l@gmail.com>
2022-03-26 09:58:04 +0100

    exercise objets

--- js/main.js
+++ js/main.js
@@ -78,12 +78,12 @@ if( kittenDescOne.includes(descrSearchText) ) {console.log()
 
 // EVENTO CLICK PARA EL +
 
-// const find = document.querySelector('.js-plus');
-// const form = document.querySelector(".new-form");
+ const find = document.querySelector('.js-plus');
+ const form = document.querySelector(".new-form");
 
-// find.addEventListener('click', () => {
-//   form.classList.toggle("collapsed");
-// });
+ find.addEventListener('click', () => {
+   form.classList.toggle("collapsed");
+ });
 
 
 // EVENTO AÑADIR
@@ -112,8 +112,8 @@ add.addEventListener('click', () => {
 
 // MOSTRAR FORMULARIO NUEVO GATITO
 
-const find = document.querySelector('.js-plus');
-const form = document.querySelector(".new-form");
+const find1 = document.querySelector('.js-plus');
+const form1 = document.querySelector(".new-form");
 
 function showMeCatForm () {
   find.classList.remove('collapsed');
@@ -130,4 +130,88 @@ form.addEventListener('click',() => {
 
 function hideNewCatForm () {
 find.classList.add('collapsed');
+}
+
+//OBJETOS
+
+
+const kittenDataOne = {
+  image: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
+  name: "Anastacio",
+  desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+  race: "British Shorthair",
+};
+const kittenDataTwo = {
+image: "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
+name: "Fiona",
+desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!",
+race: "British Shorthair",
+}
+const kittenDataThree = {
+  image:"https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
+  name: "Cielo",
+  desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!",
+  race: "British Shorthair",
+}
+
+function renderKitten(cualquierKittenData) {
+  const kittenGlobal = `<li class="card">
+  <article>
+    <img
+      class="card_img"
+      src=${cualquierKittenData.image}
+      alt="gatito"
+    />
+    <h3 class="card_title">${cualquierKittenData.name}</h3>
+    <h4 class="card_race">${cualquierKittenData.race}</h4>
+    <p class="card_description">
+      ${cualquierKittenData.desc}
+    </p>
+  </article>
+</li>`;
+list.innerHTML += kittenGlobal;
+}
+renderKitten(kittenDataOne);
+//renderKitten( {name:"sldkfjñsa", desc:"sldkfjs", race:"sdlkfsj", image: "sdlfkjs"} );
+
+/*
+  const kittenGlobal = `<li class="card">
+  <article>
+    <img
+      class="card_img"
+      src=${kittenDataOne.image}
+      alt="gatito"
+    />
+    <h3 class="card_title">${kittenDataOne.name}</h3>
+    <h4 class="card_race">${kittenDataOne.race}</h4>
+    <p class="card_description">
+      ${kittenDataOne.desc}
+    </p>
+  </article>
+</li>`;
+list.innerHTML += kittenGlobal;
+*/
+renderKitten(kittenDataTwo);
+/*
+
+  const kittenGlobal = `<li class="card">
+  <article>
+    <img
+      class="card_img"
+      src=${kittenDataTwo.image}
+      alt="gatito"
+    />
+    <h3 class="card_title">${kittenDataTwo.name}</h3>
+    <h4 class="card_race">${kittenDataTwo.race}</h4>
+    <p class="card_description">
+      ${kittenDataTwo.desc}
+    </p>
+  </article>
+</li>`;
+list.innerHTML += kittenGlobal;
+*/
+renderKitten(kittenDataThree);
+
+function renderRace (){
+
 }
